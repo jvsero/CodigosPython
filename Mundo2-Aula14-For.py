2401dac978e14c3275f82190e494584185bba586
Joel <joel.sero@aluno.senai.br>
2026-08-19 18:02:35 -0300

    Create Mundo2-Aula14-For.py

---
+++ Mundo2-Aula14-For.py
@@ -0,0 +1,9 @@
+# Estrutura de Repetição For
+
+letra=input("Informe a Váriavel:  ")
+repetir=int(input("Informe a quantidade de vezes a repetir:"))
+
+
+for quantidade in range(repetir):
+    print(quantidade,end=" ")
+print("\nFim")
