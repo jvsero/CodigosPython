86075e4e3ab0c2f55292699ee4963dd329ca0601
Joel <joel.sero@aluno.senai.br>
2026-08-24 20:18:07 -0300

    Update MatrizFor-03.py

--- MatrizFor-03.py
+++ MatrizFor-03.py
@@ -1,1 +1,16 @@
+escada=[]
+
+for i in range(1,6):
+    linha=[]
+    for j in range(i):
+        linha.append("#")
+
+    escada.append(linha)
+
+    for linha in escada:
+        print("".join(linha))
+
+
+
+
 
