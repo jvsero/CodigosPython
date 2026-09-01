59939b1bdaafd49635dbaaad8076ab8582d32f46
Joel <joel.sero@aluno.senai.br>
2026-09-01 14:25:52 -0300

    Update GeradorNumAleatorios02-importrandom.py

--- GeradorNumAleatorios02-importrandom.py
+++ GeradorNumAleatorios02-importrandom.py
@@ -1,1 +1,20 @@
 
+import random
+numeros = [ ]
+mensagem = "    \n        Banca da Sorte          \n"
+endereco = " Rua José Barreto S Sombra \n Centro Araripina-PE"
+print(mensagem)
+print(endereco)
+print("\n")
+print("---------------------------------------------------")
+print("      Gerador de Números \n    ")
+
+while len(numeros)<10:
+    numero= random.randint(1,2000)
+    numeros.append(numero)
+    print(f"Número: {numero}")
+
+print("\n Últimos 10 números chamados Banca da Sorte :  ")
+
+for numero in numeros:
+    print(numero)
