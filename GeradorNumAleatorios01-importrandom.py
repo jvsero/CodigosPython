dbc17ab510b78e01954d590a5af08900071f6b2d
Joel <joel.sero@aluno.senai.br>
2026-09-01 14:10:09 -0300

    Create GeradorNumAleatorios01-importrandom.py

---
+++ GeradorNumAleatorios01-importrandom.py
@@ -0,0 +1,9 @@
+import random
+num=random.randint(1,200)
+mensagem="    \n        Banca da Sorte          \n"
+endereco=" Rua José Barreto S Sombra \n Centro Araripina-PE"
+print(mensagem)
+print(endereco)
+print("\n")
+print(f"Gerador de Números Aleatórios: {num}")
+
