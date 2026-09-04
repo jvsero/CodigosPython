4ae016d5bc4982ded45e9d625ef09b024a46f9db
Joel <joel.sero@aluno.senai.br>
2026-09-03 21:39:16 -0300

    Create Automovel-Heranca.py

---
+++ Automovel-Heranca.py
@@ -0,0 +1,19 @@
+class Automovel:
+    cor=""
+    marca=""
+    ano=""
+    velocidade_maxima=""
+    situacao=""
+   
+   # Método de Funcionamento
+    def ligar(self):
+        return
+       
+    # Método para desligar
+
+    def desligar(self):
+        return
+           
+
+
+   
